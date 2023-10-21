311310212a3d2f5ddd2c859ff45f3f25b71f5ff3
HaaZee <benj.jones7@gmail.com>
2023-10-21 13:27:46 +0100

    Added roulette board creation.

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ extensions = [
     "commands.cf",
     "commands.pay",
     "commands.stats",
-    "commands.help" # FIXME: This doesn't return any commands when it runs
+    "commands.help", # FIXME: This doesn't return any commands when it runs
+    "commands.roulette"
 ]
 
 bot.load_extensions(extensions)
